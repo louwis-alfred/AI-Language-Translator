53e9b0670b83d63a5c59e377ad2504e6874806f1
Louwis Alfred <128562373+louwis-alfred@users.noreply.github.com>
2024-07-16 23:31:11 +0800

    Add files via upload

---
+++ openaiTranslate.py/main.py
@@ -0,0 +1,23 @@
+import openai
+import os
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+openai.api_key  = os.getenv('OPENAI_API_KEY')
+
+def get_completion(prompt, model="gpt-3.5-turbo", temperature=0): 
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, 
+    )
+    return response.choices[0].message["content"]
+
+prompt = f"""
+Translate the following English text to Spanish: \ 
+```Hi, Nice to meet you my friend!```
+"""
+response = get_completion(prompt)
+print(response)
